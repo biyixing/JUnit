bc5347ec1f937e7b5d85d789008d2ad295a19233
biyixing <1345594293@qq.com>
2015-09-07 19:35:04 +0800

    项目提交

---
+++ src/biyixing/Xing.java
@@ -0,0 +1,13 @@
+package biyixing;
+
+public class Xing {
+	public void Add() {
+	}
+	public int add(int n1,int n2){
+		int sum;
+		sum=n1+n2;
+		return sum;
+		
+	}
+
+}
---
+++ src/biyixing/XingTest.java
@@ -0,0 +1,19 @@
+package biyixing;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class XingTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("ипн╢й╣ож");
+	}
+
+}
